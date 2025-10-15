a3430defd1b06082a67dbd379a7cfbf36a98b492
kaua <leguissamonk@gmail.com>
2025-10-15 17:59:45 -0400

    s

--- src/RotasProdutos/server.js
+++ src/RotasProdutos/server.js
@@ -1,24 +1,25 @@
 import express from "express"
-import {Database} from "./databases.js"
+import {Database} from "../databases.js"
 const app = express()
 
 app.use(express.json())
 const database = new Database()
 
+const produtos = []
 app.get('/', (req, res) =>{
     res.send("voce esta na pagina principal")
 })
 
-app.get('/usuario', (req, res) =>{
-    const data = database.select("usuario")
+app.get('/produto', (req, res) =>{
+    const data = database.select("produto")
     res.status(200).json(data)
 })
 
-app.post('/usuario', (req, res) =>{
-    const {nome, idade, email} = req.body
-   if(nome, idade, email){
-    database.insert("usuario", {nome, idade, email} )
-      return res.status(201).send("usuario criado")
+app.post('/produto', (req, res) =>{
+    const {nome, valor} = req.body
+   if(nome, valor){
+    database.insert("produto", {nome, valor} )
+      return res.status(201).send("produto criado")
    }
 
 return res.status(400).send("informações invalidas")
